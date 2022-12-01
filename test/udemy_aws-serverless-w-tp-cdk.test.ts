e3373d4dd7386423a91cb97ee96e79ec1fb99ed9
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2022-12-01 09:22:15 -0300

    Initial commit

---
+++ test/udemy_aws-serverless-w-tp-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UdemyAwsServerlessWTpCdk from '../lib/udemy_aws-serverless-w-tp-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/udemy_aws-serverless-w-tp-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UdemyAwsServerlessWTpCdk.UdemyAwsServerlessWTpCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
